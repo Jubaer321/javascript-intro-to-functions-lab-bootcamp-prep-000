59a825a99daf571a1b5054e3d86762464312af37
jubaerahmed12@hotmail.co.uk <jubaerahmed12@hotmail.co.uk>
2018-09-14 10:10:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  
+  string = "hello"
+  
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  
+  string = "HELLO"
+  
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  
+var upperCase = "HELLO"
+return upperCase.toUpperCase() === upperCase
+}
+
+var lowerCase = "hello"
+lowerCase.toLowerCase() === lowerCase
+
+var mixedCase = " i love you, grandma."
+mixedCase = toLowerCase() === mixedCase
+  
+}
+
+
